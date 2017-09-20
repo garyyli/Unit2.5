aa15122186c93a30c72c909e0d5ff3bdd3038d4b
garyyli <garyli@hanovernorwichschools.org>
2017-09-20 13:47:37 -0400

    Updated from Brython Server: 9/20/2017 1:47:37 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -14,8 +14,10 @@ blackOutline = LineStyle(1,black) #pixels, color
 redRectangle = RectangleAsset(200,100,blackOutline,red) #width, heright, outline, fill
 blueCircle = CircleAsset(50,blackOutline, blue) #radius, outline, fill
 greenEllipse = EllipseAsset(100,50,blackOutline,green) #horizontalradius, verticalradius, outline, fill
+blackLine = LineAsset(50, 160, blackOutline) #xEndpoint, yEndpoint, lineStyle
 
 Sprite(redRectangle)
 Sprite(blueCircle, (50,50))
 Sprite(greenEllipse, (200,400))
+Sprite(blackLine)
 App().run()

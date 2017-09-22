b1b041a9cbf61b09ebeb0402471f3dd5236a2161
garyyli <garyli@hanovernorwichschools.org>
2017-09-22 14:52:25 -0400

    Updated from Brython Server: 9/22/2017 2:52:26 PM

--- house.py
+++ house.py
@@ -12,14 +12,10 @@ black = Color(0x000000,1)
 blackOutline = LineStyle(1,black) #pixels, color
 
 redRectangle = RectangleAsset(200,100,blackOutline,red) #width, heright, outline, fill
-blueCircle = CircleAsset(100,blackOutline, blue) #radius, outline, fill
-greenEllipse = EllipseAsset(100,50,blackOutline,green) #horizontalradius, verticalradius, outline, fill
 blackLine = LineAsset(50, 160, blackOutline) #xEndpoint, yEndpoint, lineStyle
-redTriangle = PolygonAsset([(0,0), (120,180), (60,300)],blackOutline, red) #list of endpoints
+redTriangle = PolygonAsset([(100,150), (200,50), (300,150)],blackOutline, red) #list of endpoints
 
 Sprite(redRectangle)
-Sprite(blueCircle, (200,200))
-Sprite(greenEllipse, (200,400))
 Sprite(blackLine)
 Sprite(redTriangle)
 App().run()

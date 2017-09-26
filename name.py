b55327127c301d67e6d57f1d81290b114a440143
garyyli <garyli@hanovernorwichschools.org>
2017-09-26 14:52:27 -0400

    Updated from Brython Server: 9/26/2017 2:52:27 PM

--- name.py
+++ name.py
@@ -1,1 +1,11 @@
+#Gary Li
+#9/26/17
+#name.py
+
+from ggame import *
+
+name = input('Enter your name: ')
+color = input('Enter an RGB color code: ')
+
+text = TextAsset('GAAAAAAAAAAAAA', fill=blue,style='bold 30pt Times')
 

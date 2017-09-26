5eb23491ec5c2202260ca65f4bc29c0599500367
garyyli <garyli@hanovernorwichschools.org>
2017-09-26 14:29:33 -0400

    Updated from Brython Server: 9/26/2017 2:29:33 PM

--- house.py
+++ house.py
@@ -5,17 +5,16 @@
 from ggame import *
 
 red = Color(0xFF0000,1)
-green = Color(0x00FF00,1)
-blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 
 blackOutline = LineStyle(1,black) #pixels, color
 
 redRectangle = RectangleAsset(125,150,blackOutline,red) #width, heright, outline, fill
-blackLine = LineAsset(50, 160, blackOutline) #xEndpoint, yEndpoint, lineStyle
 redTriangle = PolygonAsset([(100,150), (200,50), (300,150)],blackOutline, red) #list of endpoints
+windows = RectangleAsset(50,50,blackOutline,black) #width, heright, outline, fill
 
 Sprite(redRectangle, (140,150))
-Sprite(blackLine)
 Sprite(redTriangle)
+Sprite(windows)
+
 App().run()

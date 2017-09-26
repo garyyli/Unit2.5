a243882c0c13c4287c442199f1e3385f476ce68d
garyyli <garyli@hanovernorwichschools.org>
2017-09-26 14:46:03 -0400

    Updated from Brython Server: 9/26/2017 2:46:03 PM

--- germany.py
+++ germany.py
@@ -8,13 +8,13 @@ red = Color(0xFF0000,1)
 black = Color(0x000000,1)
 yellow = Color(0xFFFF00,1)
 
-blackOutline = LineStyle(1,black) #pixels, color
-redOutline = LineStyle(1,red) #pixels, color
-yellowOutline = LineStyle(1,yellow) #pixels, color
+blackOutline = LineStyle(1,black)
+redOutline = LineStyle(1,red)
+yellowOutline = LineStyle(1,yellow)
 
-blackRectangle = RectangleAsset(500,100,blackOutline,black) #width, heright, outline, fill
-redRectangle = RectangleAsset(500,100,redOutline,red) #width, heright, outline, fill
-yellowRectangle = RectangleAsset(500,100,yellowOutline,yellow) #width, heright, outline, fill
+blackRectangle = RectangleAsset(500,100,blackOutline,black)
+redRectangle = RectangleAsset(500,100,redOutline,red)
+yellowRectangle = RectangleAsset(500,100,yellowOutline,yellow)
 
 Sprite(blackRectangle)
 Sprite(redRectangle,(0,100))

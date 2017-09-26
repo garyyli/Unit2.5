e04bc43917b972b5f1fa66391eb3d9b356465b85
garyyli <garyli@hanovernorwichschools.org>
2017-09-26 14:32:07 -0400

    Updated from Brython Server: 9/26/2017 2:32:07 PM

--- house.py
+++ house.py
@@ -12,12 +12,12 @@ blackOutline = LineStyle(1,black) #pixels, color
 redRectangle = RectangleAsset(125,150,blackOutline,red) #width, heright, outline, fill
 redTriangle = PolygonAsset([(100,150), (200,50), (300,150)],blackOutline, red) #list of endpoints
 windows = RectangleAsset(35,35,blackOutline,black) #width, heright, outline, fill
-door = RectangleAsset(20,35,blackOutline,black) #width, heright, outline, fill
+door = RectangleAsset(35,50,blackOutline,black) #width, heright, outline, fill
 
 Sprite(redRectangle, (140,150))
 Sprite(redTriangle)
 Sprite(windows, (150, 175))
 Sprite(windows, (220, 175))
-Sprite(door)
+Sprite(door, (180, 250))
 
 App().run()
